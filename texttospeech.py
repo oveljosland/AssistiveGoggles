51b3e02927f59452ae53226bd99da630c23a36be
YoPotato80 <mathias.schiotz@hotmail.no>
2024-10-18 12:50:45 +0200

    fjerna kode fra tts som allerede var i self_calibrate

--- texttospeech.py
+++ texttospeech.py
@@ -17,20 +17,20 @@ engine.setProperty('volume', 0.9)
 pipeline = rs.pipeline()
 config = rs.config()
 
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
+## Get device product line for setting a supporting resolution
+#pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+#pipeline_profile = config.resolve(pipeline_wrapper)
+#device = pipeline_profile.get_device()
+#device_product_line = str(device.get_info(rs.camera_info.product_line))
+#
+#found_rgb = False
+#for s in device.sensors:
+#    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+#        found_rgb = True
+#        break
+#if not found_rgb:
+#    print("The demo requires Depth camera with Color sensor")
+#    exit(0)
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
