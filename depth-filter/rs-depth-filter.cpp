7314f64046fe1e28d1e5bb3110f8cdfdae96c26e
Ove Ljosland <oveljosland@icloud.com>
2024-11-01 12:22:57 +0100

    rs

--- depth-filter/rs-depth-filter.cpp
+++ depth-filter/rs-depth-filter.cpp
@@ -1,3 +1,4 @@
+// License: Apache 2.0. See LICENSE file in root directory.
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <librealsense2/rs_advanced_mode.hpp>
 #include <opencv2/opencv.hpp>   // Include OpenCV API
