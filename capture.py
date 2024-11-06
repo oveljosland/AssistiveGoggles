ed1f2f7e3a52c0581ec63b0b8dfb2ff50635c221
JustaHollowDot <emil@hallingstad.me>
2024-11-06 18:45:36 +0100

    implemented file using the class given in realsense_camera.py

--- capture.py
+++ capture.py
@@ -1,11 +1,39 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
-
+import realsense_camera
 
 #TODO: implement this using the camera class
 #TODO: Move all program logic into functions
 
+def main():
+    camera = realsense_camera.PyRealSenseCamera()
+
+    while True:
+        camera.wait_for_next_frame()
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(np.asanyarray(camera.depth_image.get_data()), alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = camera.color_image.shape
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(camera.color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((camera.color_image, depth_colormap))
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
@@ -13,7 +41,8 @@ config = rs.config()
 # Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
+# device = pipeline_profile.get_device()
+device: rs.device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
 found_rgb = False
@@ -33,7 +62,6 @@ pipeline.start(config)
 
 try:
     while True:
-
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
@@ -67,3 +95,4 @@ finally:
 
     # Stop streaming
     pipeline.stop()
+"""
