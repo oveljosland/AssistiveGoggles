9dd6b3bd56ae0bcf7f083a4b933c9f623348a180
Ove Ljosland <oveljosland@icloud.com>
2024-11-15 09:47:13 +0100

    Refactor object detection pipeline and add fall detection functionality

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import subprocess
-from viewer import show
-from self_calibrate import main as camera_calibrate
 
 def main():
     try:
-        camera_calibrate()
+        # oppsett her
         while True:
-            show()
-            dnn_response = subprocess.run(["dnn/rs_dnn"], capture_output=True, text=True)
-            print(dnn_response.stdout)
-            print(dnn_response.stderr)
+            
     except KeyboardInterrupt:
         print("Exiting...")
     
--- object-detection/detect_object.py
+++ object-detection/detect_object.py
@@ -28,14 +28,15 @@ nn = cv.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 print("Starter videostrøm...")
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 15)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start(config)
 time.sleep(2)  # la kamera 'varme opp'
 
 try:
     while True:
         # hent frames
-        frames = pipeline.wait_for_frames(timeout_ms=10000)
+        frames = pipeline.wait_for_frames()
+        #frames = pipeline.wait_for_frames(timeout_ms=10000)
         color_frame = frames.get_color_frame()
         if not color_frame:
             continue
